961e571cdba77d1ee2aff9a889fb9afbda0e499d
anasofiasanchez0716 <anasofiasanchez0716@gmail.com>
2026-04-09 12:41:15 -0500

    mis cambios

--- aplicacion/views.py
+++ aplicacion/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+def inicio(request):
+    if request.method == "POST":
+        user=request.POST.get("username")
+        password=request.POST.get("password")
+
+        # Validación básica (sin seguridad)
+        if user== "admin" and password == "1234":
+            return redirect("home")
+        else:
+            return render(request, "inicio.html", {"error": "Datos incorrectos"})
+
+    return render(request, "inicio.html")
 # Create your views here.
