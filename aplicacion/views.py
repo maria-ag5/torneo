d74089540be8f8301c65ae446efadbb645286941
anasofiasanchez0716 <anasofiasanchez0716@gmail.com>
2026-04-10 09:11:47 -0500

    cambios ana

--- aplicacion/views.py
+++ aplicacion/views.py
@@ -5,10 +5,10 @@ def inicio(request):
         user=request.POST.get("username")
         password=request.POST.get("password")
     
-        if user== "admin" and password == "1234":
+        if user== "cosita" and password == "1234":
             return redirect("home")
         else:
             return render(request, "inicio.html", {"error": "Datos incorrectos"})
 
     return render(request, "inicio.html")
-# Create your views here.
+
